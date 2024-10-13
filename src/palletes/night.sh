d711e8776d70e986d5baff40132a2ab7aa4ecc5f
Daniel Hoffmann <daniel.hoffmann@cth24.de>
2024-10-13 13:56:27 +0200

    Optimized Panes

--- src/palletes/night.sh
+++ src/palletes/night.sh
@@ -2,37 +2,55 @@
 
 #Tokyo Night Pallet
 declare -A PALLETE=(
-  [none]="NONE"
-  [bg]="#1a1b26"
-  [bg_dark]="#16161e"
-  [bg_highlight]="#292e42"
-  [terminal_black]="#414868"
-  [fg]="#c0caf5"
-  [fg_dark]="#a9b1d6"
-  [fg_gutter]="#3b4261"
-  [dark3]="#545c7e"
-  [comment]="#565f89"
-  [dark5]="#737aa2"
-  [blue0]="#3d59a1"
-  [blue]="#7aa2f7"
-  [cyan]="#7dcfff"
-  [blue1]="#2ac3de"
-  [blue2]="#0db9d7"
-  [blue5]="#89ddff"
-  [blue6]="#b4f9f8"
-  [blue7]="#394b70"
-  [magenta]="#bb9af7"
-  [magenta2]="#ff007c"
-  [purple]="#9d7cd8"
-  [orange]="#ff9e64"
-  [yellow]="#e0af68"
-  [green]="#9ece6a"
-  [green1]="#73daca"
-  [green2]="#41a6b5"
-  [teal]="#1abc9c"
-  [red]="#f7768e"
-  [red1]="#db4b4b"
-  [white]="#ffffff"
+    [none]="NONE"
+    [bg]="#1a1b26"
+    [bg_dark]="#16161e"
+    [terminal_black]="#414868"
+    [fg]="#c0caf5"
+    [fg_dark]="#a9b1d6"
+    [fg_gutter]="#3b4261"
+    [dark3]="#545c7e"
+    [comment]="#565f89"
+    [dark5]="#737aa2"
+    [blue0]="#3d59a1"
+    [blue]="#7aa2f7"
+    [cyan]="#7dcfff"
+    [blue1]="#2ac3de"
+    [blue2]="#0db9d7"
+    [blue5]="#89ddff"
+    [blue6]="#b4f9f8"
+    [blue7]="#394b70"
+    [magenta]="#bb9af7"
+    [magenta2]="#ff007c"
+    [purple]="#9d7cd8"
+    [orange]="#ff9e64"
+    [green]="#9ece6a"
+    [green1]="#73daca"
+    [green2]="#41a6b5"
+    [teal]="#1abc9c"
+    [red]="#f7768e"
+    [red1]="#db4b4b"
+    [white]="#ffffff"
+
+    [green]="#292e42"           # Hintergrundfarbe First Element
+    [bg_highlight]="#292e42"    # Status Bar Background
+
+    [fg_gutter]="#c0caf5"       # Font Color First Element
+
+    [blue0]="#3b4261"           # Hauptfarbe für das aktive Paneel
+    [purple]="#3b4261"          # Aktives Pane Hintergrund
+    [magenta]="#3b4261"         # Aktives Pane Hintergrund Number
+    [dark3]="#292e42"           # Inaktives Pane Hintergrund
+    [dark5]="#292e42"           # Inactives Pane Hintergrund Number
+    [blue7]="#292e42"           # Datum und Zeit
+    [white]="#c0caf5"           # Schriftfarbe aktive und inaktive Panes
+
+    [red]="#000000"             # Tmux Command - Hintergrund
+    [bg_dark]="#c0caf5"         # Tmux Command - Schriftfarbe
+
+    [yellow]="#000000"          # Command Mode Background
+    [green2]="#41a6b5"          # Farbe für aktive Befehle
+
 )
 
 export PALLETE
--- src/utils.sh
+++ src/utils.sh
@@ -1,62 +1,80 @@
 #!/usr/bin/env bash
 
 function get_tmux_option() {
-	local option=$1
-	local default_value=$2
-	local -r option_value=$(tmux show-option -gqv "$option")
+    local option=$1
+    local default_value=$2
+    local -r option_value=$(tmux show-option -gqv "$option")
 
-	if [ -z "$option_value" ]; then
-		echo "$default_value"
-	else
-		echo "$option_value"
-	fi
+    if [ -z "$option_value" ]; then
+        echo "$default_value"
+    else
+        echo "$option_value"
+    fi
 }
 
 function generate_left_side_string() {
 
-	session_icon=$(get_tmux_option "@theme_session_icon" "⋅")
-	if [ "$transparent" = "true" ]; then
-		local separator_end="#[bg=default]#{?client_prefix,#[fg=${PALLETE[yellow]}],#[fg=${PALLETE[green]}]}${left_separator:?}#[none]"
-	else
-		local separator_end="#[bg=${PALLETE[bg_highlight]}]#{?client_prefix,#[fg=${PALLETE[yellow]}],#[fg=${PALLETE[green]}]}${left_separator:?}#[none]"
-	fi
+    session_icon=$(get_tmux_option "@theme_session_icon" "⋅")
+    if [ "$transparent" = "true" ]; then
+        local separator_end="#[bg=default]#{?client_prefix,#[fg=${PALLETE[yellow]}],#[fg=${PALLETE[green]}]}${left_separator:?}#[none]"
+    else
+        local separator_end="#[bg=${PALLETE[bg_highlight]}]#{?client_prefix,#[fg=${PALLETE[yellow]}],#[fg=${PALLETE[green]}]}${left_separator:?}#[none]"
+    fi
 
-	echo "#[fg=${PALLETE[fg_gutter]},bold]#{?client_prefix,#[bg=${PALLETE[yellow]}],#[bg=${PALLETE[green]}]} ${session_icon} #S ${separator_end}"
+    echo "#[fg=${PALLETE[fg_gutter]},bold]#{?client_prefix,#[bg=${PALLETE[yellow]}],#[bg=${PALLETE[green]}]} ${session_icon} #S ${separator_end}"
 }
 
 function generate_inactive_window_string() {
 
-	inactive_window_icon=$(get_tmux_option "@theme_plugin_inactive_window_icon" " ")
-	zoomed_window_icon=$(get_tmux_option "@theme_plugin_zoomed_window_icon" " ")
+    active_window_icon=$(get_tmux_option "@theme_plugin_active_window_icon" " ")
+    zoomed_window_icon=$(get_tmux_option "@theme_plugin_zoomed_window_icon" " ")
+    pane_synchronized_icon=$(get_tmux_option "@theme_plugin_pane_synchronized_icon" "✵")
 
-	if [ "$transparent" = "true" ]; then
-		local separator_start="#[bg=default,fg=${PALLETE['dark5']}]${left_separator_inverse}#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]"
-		local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
-		local separator_end="#[bg=default,fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
-	else
-		local separator_start="#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
-		local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
-		local separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
-	fi
+    if [ "$transparent" = "true" ]; then
+        local separator_start="#[bg=default,fg=${PALLETE['dark5']}]${left_separator_inverse}#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]"
+        local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
+        local separator_end="#[bg=default,fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
+    else
+        local separator_start="#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
+        local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['dark5']}]${left_separator:?}#[none]"
+        local separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
+    fi
 
-	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$inactive_window_icon}#W ${separator_end}"
+    window_index="#[fg=${PALLETE[white]}] #I"
+    window_status="#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$active_window_icon}"
+    window_name="#[fg=${PALLETE['white']},bold]#(basename #{pane_current_path})"
+    current_command="#[fg=#56B6C2]#{pane_current_command}"
+    sync_status="#{?pane_synchronized,$pane_synchronized_icon,}"
+    assembled_string="${separator_start}${window_index}${separator_internal}${window_status}${window_name} ${current_command} ${sync_status}${separator_end}"
+
+    echo "${assembled_string}"
 }
-
 function generate_active_window_string() {
+    # Symbole und Optionen laden
+    active_window_icon=$(get_tmux_option "@theme_plugin_active_window_icon" " ")
+    zoomed_window_icon=$(get_tmux_option "@theme_plugin_zoomed_window_icon" " ")
+    pane_synchronized_icon=$(get_tmux_option "@theme_plugin_pane_synchronized_icon" "✵")
 
-	active_window_icon=$(get_tmux_option "@theme_plugin_active_window_icon" " ")
-	zoomed_window_icon=$(get_tmux_option "@theme_plugin_zoomed_window_icon" " ")
-	pane_synchronized_icon=$(get_tmux_option "@theme_plugin_pane_synchronized_icon" "✵")
+    # Separatoren je nach Transparenz einstellen
+    if [ "$transparent" = "true" ]; then
+        separator_start="#[bg=default,fg=${PALLETE['magenta']}]${left_separator_inverse}#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]"
+        separator_start="#[bg=default,fg=${PALLETE['magenta']}]${left_separator_inverse}#[bg=${PALLETE['magenta']},fg=#000000]"
+        separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
+        separator_end="#[bg=default,fg=${PALLETE['purple']}]${left_separator:?}#[none]"
+    else
+        separator_start="#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
+        separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
+        separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['purple']}]${left_separator:?}#[none]"
+    fi
 
-	if [ "$transparent" = "true" ]; then
-		separator_start="#[bg=default,fg=${PALLETE['magenta']}]${left_separator_inverse}#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]"
-		separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
-		separator_end="#[bg=default,fg=${PALLETE['purple']}]${left_separator:?}#[none]"
-	else
-		separator_start="#[bg=${PALLETE['magenta']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
-		separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
-		separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['purple']}]${left_separator:?}#[none]"
-	fi
+    window_index="#[fg=#ffffff] #I"
+    window_status="#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$active_window_icon}"
+    window_name="#[fg=${PALLETE['white']},bold]#(basename #{pane_current_path})"
+    current_command="#[fg=#56B6C2]#{pane_current_command}"
+    sync_status="#{?pane_synchronized,$pane_synchronized_icon,}"
+    # Alle Teile zusammenbauen
+    assembled_string="${separator_start}${window_index}${separator_internal}${window_status}${window_name} ${current_command} ${sync_status}${separator_end}"
 
-	echo "${separator_start}#[fg=${PALLETE[white]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag,$zoomed_window_icon,$active_window_icon}#W #{?pane_synchronized,$pane_synchronized_icon, }${separator_end}#[none]"
+    echo "${assembled_string}"
 }
+
